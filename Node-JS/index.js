5cba7ef48b88119241c5f1df3eac9c36eaa4e115
Disha881 <88449387+Disha881@users.noreply.github.com>
2022-09-19 14:09:33 +0530

    Add files via upload

---
+++ Node-JS/index.js
@@ -0,0 +1,10 @@
+const calculator = require('./calc');
+
+let r  = calculator.sum(2,3)
+console.log(r)
+let s  = calculator.sum(2,3)
+console.log(s)
+let t  = calculator.sum(2,3)
+console.log(t)
+let u  = calculator.sum(2,3)
+console.log(u)
